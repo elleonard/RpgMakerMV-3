f855d5801c34dd0c6aca16ee2251325c907569d6
munokura <munokura@users.noreply.github.com>
2020-04-02 08:29:12 +0900

    関数化

--- MNKR_plugins/MNKR_MOG_BattleHud_ChangeActor.js
+++ MNKR_plugins/MNKR_MOG_BattleHud_ChangeActor.js
@@ -2,23 +2,41 @@
  * @plugindesc 発動スイッチがONの時、アクターの表示画像を指定番号画像に切り替えます。
  * @author munokura
  *
- * @param Switch Id
+ * @param Switch Id 1
  * @text 発動スイッチID
  * @type switch
  * @desc 発動させるスイッチID
  * @default 11
  *
- * @param Before Face Id
+ * @param Before Face Id 1
  * @text 変更前アクター画像ID
  * @type number
  * @desc 変更前アクター画像ID
  * @default 1
  *
- * @param After Face Id
+ * @param After Face Id 1
  * @text 変更後アクター画像ID
  * @type number
  * @desc 変更後アクター画像ID
  * @default 3
+ *
+ * @param Switch Id 2
+ * @text 発動スイッチID
+ * @type switch
+ * @desc 発動させるスイッチID
+ * @default 12
+ *
+ * @param Before Face Id 2
+ * @text 変更前アクター画像ID
+ * @type number
+ * @desc 変更前アクター画像ID
+ * @default 2
+ *
+ * @param After Face Id 2
+ * @text 変更後アクター画像ID
+ * @type number
+ * @desc 変更後アクター画像ID
+ * @default 4
  *
  * @help
  * MOG_BattleHud (v5.04)用のパッチプラグインです。
@@ -43,38 +61,50 @@
 	var beforeFaceId2 = Number(parameters['Before Face Id 2'] || 1);
 	var afterFaceId2 =  String(parameters['After Face Id 2'] || 3);
 
+	function changeFace(faceId) {
+		if ($gameSwitches.value(switchId1) && (faceId === beforeFaceId1)) {
+			return "Face_" + afterFaceId1;
+		} else if ($gameSwitches.value(switchId2) && (faceId === beforeFaceId2)) {
+			return "Face_" + afterFaceId2;
+		};
+	return "Face_" + faceId;
+    };
+
 //==============================
 // * Create Face
 //==============================
-	const _Battle_Hud_create_face = Battle_Hud.prototype.create_face;
 	Battle_Hud.prototype.create_face = function() {
-			if (String(Moghunter.bhud_face_visible) != "true") {
-				return;
-			};
-			this.removeChild(this._face);
-			if (!this._battler) {
-				return;
-			};
-			if($gameSwitches.value(switchId) && (this._battler._actorId === beforeFaceId)){
-				this._face = new Sprite(ImageManager.loadBHud("Face_" + afterFaceId));
-				this._face.anchor.x = 0.5;
-				this._face.anchor.y = 0.5;
-				this._face_data = [0,0,false,false,false,-1];
-				this._face.ph = 0;
-				this._face.animation = [-1,0,0,0,0,0,0,0,0];
-				this._face.breathEffect = this._battler._bhud.faceBreath;
-				this._face.scaleY = 0;
-				if (String(Moghunter.bhud_face_shake) === "true") {
-					this._face_data[2] = true;
-				};
-				if (String(Moghunter.bhud_face_animated) === "true") {
-					this._face_data[4] = true;
-				};
-				this._battler._bhud_face_data = [0,0,0,1];
-				this.addChild(this._face);
-			} else {
-			_Battle_Hud_create_face.apply(this, arguments);
+		if (String(Moghunter.bhud_face_visible) != "true") {
+			return;
 		};
+
+		this.removeChild(this._face);
+
+		if (!this._battler) {
+			return;
+		};
+
+		var actorFace = this._battler._actorId;
+		var changeFaceId = changeFace(actorFace);
+		this._face = new Sprite(ImageManager.loadBHud(changeFaceId));
+		this._face.anchor.x = 0.5;
+		this._face.anchor.y = 0.5;
+		this._face_data = [0,0,false,false,false,-1];
+		this._face.ph = 0;
+		this._face.animation = [-1,0,0,0,0,0,0,0,0];
+		this._face.breathEffect = this._battler._bhud.faceBreath;
+		this._face.scaleY = 0;
+
+		if (String(Moghunter.bhud_face_shake) === "true") {
+			this._face_data[2] = true;
+		};
+
+		if (String(Moghunter.bhud_face_animated) === "true") {
+			this._face_data[4] = true;
+		};
+
+		this._battler._bhud_face_data = [0,0,0,1];
+		this.addChild(this._face);
 	};
 
 })();
