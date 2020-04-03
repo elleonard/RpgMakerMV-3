f360af79a5ef3454d94fa04145bd529d6bbf09a4
munokura <36838778+munokura@users.noreply.github.com>
2020-04-03 11:53:05 +0900

    MOG Battle HUD v5.04 に対応変更

--- MNKR_plugins/MOGHexpan.js
+++ MNKR_plugins/MOGHexpan.js
@@ -1,21 +1,20 @@
-//============================================================================
+//=============================================================================
 // MOGHexpan.js
-//============================================================================
+//=============================================================================
 
 /*:ja
- * @plugindesc ver1.01 MOG_BattleHud(v4.0)拡張
- * @author まっつＵＰ
- * 
+ * @plugindesc ver1.02 MOG_BattleHud(v5.04)拡張
+ * @author まっつＵＰ (改変:munokura)
+ *
  * @help
- * 
  * RPGで笑顔を・・・
- * 
+ *
  * このヘルプとパラメータの説明をよくお読みになってからお使いください。
- * 
+ *
  * Moghunter様作のMOG_BattleHudに改変を加えます。
  * MOG_BattleHudよりも下に導入してください。
  * 主にプラグインコマンドの追加をします。
- * 
+ *
  * ＜戦闘フェイス画像を変更する場合＞
  * （プラグインコマンド）
  * MOGH update x y
@@ -25,19 +24,17 @@
  * このプラグインコマンドのよる画像変更は
  * その戦闘中のみ適用されます。
  * （次の戦闘開始時は再び初期設定の画像を表示）
- * 
- * 例：
- * MOGH update 20 100
- * 変数ID20の値を1にした後
+ *
+ * 例：変数ID20の値を1にした後
  * 変数ID20の値と同じIDのアクターの戦闘フェイス画像をFace_ 101.pngにする。
  * (この時、ID1のアクターが指定される)
- * 
+ * MOGH update 20 100
+ *
  * ＜戦闘フェイス画像を戦闘開始時のものにする場合＞
  * （プラグインコマンド）
+ * 例：変数ID20の値と同じIDのアクターの戦闘フェイス画像を戦闘開始時のものに戻す。
  * MOGH update 20
- * 例：
- * 変数ID20の値と同じIDのアクターの戦闘フェイス画像を戦闘開始時のものに戻す。
- * 
+ *
  * 利用規約(2020/04/03変更)：
  * この作品は マテリアル・コモンズ・ブルー・ライセンスの下に提供されています。
  * https://materialcommons.tk/mtcm-b-summary/
@@ -45,13 +42,12 @@
  *
  */
 
-
 var parameters = PluginManager.parameters('MOGHexpan');
 
 var MOGHupdateactorId = 0;
 var MOGHupdateaddnum = 0;
 
-var _MOGH_pluginCommand = Game_Interpreter.prototype.pluginCommand
+var _MOGH_pluginCommand = Game_Interpreter.prototype.pluginCommand;
 Game_Interpreter.prototype.pluginCommand = function(command, args) {
 	_MOGH_pluginCommand.call(this, command, args);
 	if(command === 'MOGH'){
@@ -69,36 +65,44 @@ Game_Interpreter.prototype.pluginCommand = function(command, args) {
 //一応フレームアニメーションの処理内容はフックで残す。
 var _MOGH_update_face_animation = Battle_Hud.prototype.update_face_animation;
 Battle_Hud.prototype.update_face_animation = function() {
-	if(MOGHupdateaddnum > 0 && MOGHupdateactorId === this._battler._actorId){
+	if (MOGHupdateaddnum > 0 && MOGHupdateactorId === this._battler._actorId){
 		this.create_faceMOGH();
 	};
 	_MOGH_update_face_animation.call(this);
 };
 
 Battle_Hud.prototype.create_faceMOGH = function() {
+
 	if (String(Moghunter.bhud_face_visible) != "true") {
 		return;
 	};
-
-	var index = this.children.indexOf(this._face);
+	
 	this.removeChild(this._face);
+	
+	if (!this._battler) {
+		return;
+	};
+	
 	this._face = new Sprite(ImageManager.loadBHud("Face_" + MOGHupdateaddnum));
 	this._face.anchor.x = 0.5;
 	this._face.anchor.y = 0.5;
 	this._face_data = [0,0,false,false,false,-1];
+	this._face.ph = 0;
+	this._face.animation = [-1,0,0,0,0,0,0,0,0];
+	this._face.breathEffect = this._battler._bhud.faceBreath;
+	this._face.scaleY = 0;
 
 	if (String(Moghunter.bhud_face_shake) === "true") {
-		this._face_data[2] = true;
+		this._face_data[2] = true
 	};
-
 	if (String(Moghunter.bhud_face_animated) === "true") {
-		this._face_data[4] = true;
+		this._face_data[4] = true
 	};
 
-	this._battler._bhud_face_data = [0,0,0,0];
-	this.addChildAt(this._face,index);
+	this._battler._bhud_face_data = [0,0,0,1];
+	this.addChild(this._face);
 	this.refresh_position();
 	MOGHupdateactorId = 0;
 	MOGHupdateaddnum = 0;
-};
 
+};
